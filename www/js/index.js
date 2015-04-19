6301750d6420c864647a53cf3e8cf4076c1a5da7
Vladimir Tonkonogov <tonk0006@algonquinlive.com>
2015-04-18 21:35:39 -0400

    Moved received event function to end of script

--- www/js/index.js
+++ www/js/index.js
@@ -38,25 +38,6 @@ var app = {
         app.loadPage(null);
     },
 
-    // deviceready Event Handler function
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function (id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    },
-
     // Handle the click event
 
     handleNav: function (ev) {
@@ -94,6 +75,19 @@ var app = {
                 }
             }
         }
+    },
+    
+    // deviceready Event Handler function
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function () {
+        app.receivedEvent('deviceready');
+    },
+    
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+        
     }
 
 };
